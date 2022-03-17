97392a846c022409ad30ff6d369094a584ce76c1
Ronald Kolum <kolumronnie@gmail.com>
2022-03-17 10:55:28 +0300

    added NewsImplementationDAO logic

--- src/main/java/models/dao/NewsImplementationDAO.java
+++ src/main/java/models/dao/NewsImplementationDAO.java
@@ -1,5 +1,50 @@
 package models.dao;
 
-public class NewsImplementationDAO implements NewsDAO{
+import models.pojos.News;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsImplementationDAO implements NewsDAO{
+    private final Sql2o sql2o;
+
+    public NewsImplementationDAO( Sql2o sql2o){
+        this.sql2o = sql2o;
+    }
+
+
+    @Override
+    public void addNews(News news) {
+        String sql = "INSERT INTO news (headline, content, department_id) VALUES (:headline, :content, :department_id)";
+        try(Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql, true)
+                    .bind(news)
+                    .executeUpdate()
+                    .getKey();
+            news.setNewsId(id);
+        }
+        catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+
+    }
+
+    @Override
+    public List<News> getGeneralNews() {
+        return null;
+    }
+
+    @Override
+    public List<News> getDepartmentNews(int departmentId) {
+        List<News> allDeptNews = new ArrayList<>();
+        try (Connection con = sql2o.open()) {
+            allDeptNews.add(con.createQuery("SELECT * FROM news WHERE department_id = :id")
+                    .addParameter("id", departmentId)
+                    .executeAndFetchFirst(News.class));
+        }
+        return allDeptNews;
+    }
 }
