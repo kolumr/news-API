341a454ec86b9d4a37fe1ea4b6a94df1a7908d27
Ronald Kolum <kolumronnie@gmail.com>
2022-03-17 10:39:15 +0300

    added DepartmentsDAO logic

--- src/main/java/models/dao/DepartmentsDAO.java
+++ src/main/java/models/dao/DepartmentsDAO.java
@@ -1,4 +1,13 @@
 package models.dao;
 
+import models.pojos.Departments;
+
+import java.util.List;
+
 public interface DepartmentsDAO {
+    //add department to db
+    void addDepartment(Departments department);
+
+    //Return all departments
+    List<Departments> allDepartments();
 }
