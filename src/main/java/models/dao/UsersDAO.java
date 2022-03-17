59baf16ee309ca31310152cfc38d41b9f680252f
Ronald Kolum <kolumronnie@gmail.com>
2022-03-17 10:36:41 +0300

    added UsersDAO logic

--- src/main/java/models/dao/UsersDAO.java
+++ src/main/java/models/dao/UsersDAO.java
@@ -1,4 +1,20 @@
 package models.dao;
 
+import models.pojos.Users;
+
+import java.util.List;
+
 public interface UsersDAO {
+    //add user to DB
+    void add(Users user);
+
+    //return list of all users
+    List<Users> getAll();
+
+    //return list of all users in a department
+    List<Users> getAllUsersInDepartment(int departmentId);
+
+    //return specific information about a user
+    Users getUserInfo(int userId);
+
 }
