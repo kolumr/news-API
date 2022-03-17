71e0a8a67664d25b8b50b83283b26e82febc4334
Ronald Kolum <kolumronnie@gmail.com>
2022-03-17 10:37:53 +0300

    added NewsDAO logic

--- src/main/java/models/dao/NewsDAO.java
+++ src/main/java/models/dao/NewsDAO.java
@@ -1,4 +1,16 @@
 package models.dao;
 
+import models.pojos.News;
+
+import java.util.List;
+
 public interface NewsDAO {
+    //add news item to db
+    void addNews(News news);
+
+    //return list of general news items
+    List<News> getGeneralNews();
+
+    //return departmental news
+    List<News> getDepartmentNews(int departmentId);
 }
