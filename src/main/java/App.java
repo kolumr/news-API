c34af86335ca505bec9d74219be5253bc71b89ac
Ronald Kolum <kolumronnie@gmail.com>
2022-03-17 11:29:29 +0300

    Created a route to add a department

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,13 @@
 import models.dao.DepartmentsImplemetationDAO;
 import models.dao.NewsImplementationDAO;
 import models.dao.UsersImplementationDAO;
+import models.pojos.Departments;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
-
+import static spark.Spark.*;
+import java.util.HashMap;
+import java.util.Map;
+import com.google.gson.Gson;
 public class App {
     public static void main(String[] args) {
         //creating a connection to postgres
@@ -11,11 +15,18 @@ public class App {
         Sql2o sql2o = new Sql2o(connectionString, "postgres", "");
         Connection conn = sql2o.open();
 
-        //Instantiating DAO implementations
+        //Instantiating DAO implementations and Gson
         DepartmentsImplemetationDAO departmentsImplemetationDAO = new DepartmentsImplemetationDAO(sql2o);
         NewsImplementationDAO newsImplementationDAO = new NewsImplementationDAO(sql2o);
         UsersImplementationDAO usersImplementationDAO = new UsersImplementationDAO(sql2o);
+        Gson gson = new Gson();
 
+        //adding a new department
+        post("/departments/new", "application/json", (request, response) -> {
+            Departments department = gson.fromJson(request.body(),Departments.class);
+            departmentsImplemetationDAO.addDepartment(department);
+            return gson.toJson(department);
+        });
 
     }
 }
